0b0d106db8735ecc825c3eab01f79b8597e5e313
Nisa Nur Bayrak <nisanurbayrak01@gmail.com>
2022-09-03 23:49:18 +0300

    ödev

---
+++ PatikaDevOdev/IntegerIkililerinToplamı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerIkililerinToplamı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 2, 3, 1, 5, 2, 5, 3, 3 };
+            for (int i = 0; i < dizi.Length - 1; i++)
+            {
+                if (dizi[i] == dizi[i+1])
+                {
+                    double toplam = dizi[i] + dizi[i + 1];
+                    double kare = Math.Pow(toplam, 2);
+                    Console.WriteLine(kare);
+                    i++;
+                }
+                else
+                {
+                    int toplam = dizi[i] + dizi[i + 1];
+                    Console.WriteLine(toplam);
+                    i++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
