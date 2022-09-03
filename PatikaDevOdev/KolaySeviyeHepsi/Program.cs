b366f4b03b7652338bfb4da367f018b629c6c598
Nisa Nur Bayrak <nisanurbayrak01@gmail.com>
2022-09-03 22:03:40 +0300

    update

---
+++ PatikaDevOdev/KolaySeviyeHepsi/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Linq;
+
+namespace KolaySeviyeHepsi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    System.Console.WriteLine("Fibonacci derlemesi için sayısı giriniz");
+            //    int sayi = Int32.Parse(Console.ReadLine());
+            //    Dizi(sayi);
+            Ucgen();
+            //Silme();
+            Reverse();
+            Console.Read();
+        }
+        public static void Dizi(int len)
+        {
+            int d1 = 0;
+            int d2 = 1;
+            int d3 = 0;
+            List <int> list = new List<int>();
+            for (int i = 0; i < len; i++)
+            {
+                d3 = d1 + d2;
+                list.Add(d3);
+                d1 = d2;
+                d2 = d3;
+                var result = list.Sum();
+            }
+        }
+        public static void Ucgen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("^");
+                }
+                Console.WriteLine("");
+            }
+        }
+        public static void Silme()
+        {
+            string[] sMetin;
+            Console.Write("Metin: ");
+            string s = Console.ReadLine();
+            sMetin = s.Split(','); for (int i = 0; i < sMetin[0].Length; i++)
+            {
+                if (i != Convert.ToInt32(s.Length))
+                {
+                    var find = s.IndexOf(",");
+                    s = s.Remove(find, find + 1);
+                    i++;
+                }
+                Console.WriteLine(s);
+            }
+        }
+        public static void Reverse()
+        {
+            Console.WriteLine("Cümle: ");
+            string cumle = Console.ReadLine();
+            for (int i = cumle.Length - 1; i >= 0; i--)
+            {
+                Console.Write(cumle[i]);
+            }
+        }
+    }
+}
---
+++ PatikaDevOdev/ToDoUygulamasi/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1)Kart Ekle\r\n2)Kart Güncelle\r\n3)Kart Sil\r\n4)Board Listeleme");
+            string secim = Console.ReadLine();
+            List<string> ToDoList = new List<string>();
+            TodoL todol = new TodoL();
+            
+            switch (secim)
+            {
+                case "1":
+                    Console.Write("Başlık: ");
+                    todol.Baslik = Console.ReadLine();
+                    Console.Write("İçerik: ");
+                    todol.Icerik = Console.ReadLine(); 
+                    Console.Write("Atanan Kişi: ");
+                    todol.AtananKisi = Console.ReadLine(); 
+                    Console.Write("Büyüklük (XS, S, M, L, XL): ");
+                    todol.Buyukluk = Console.ReadLine().ToUpper();
+                    ToDoList.Add(todol.Baslik);
+                    ToDoList.Add(todol.Icerik);
+                    ToDoList.Add(todol.AtananKisi);
+                    ToDoList.Add(todol.Buyukluk);
+                    break;
+
+                    case "2":
+                    Console.Write("Güncellemek istediğiniz başlığı giriniz: ");
+                    string GuncellenecekAd = Console.ReadLine();
+                    int count = 0;
+                    foreach (var item in ToDoList)
+                    {
+                        if (item.Contains(GuncellenecekAd))
+                        {
+                            Console.Write("Başlık: ");
+                            todol.Baslik = Console.ReadLine();
+                            Console.Write("İçerik: ");
+                            todol.Icerik = Console.ReadLine();
+                            Console.Write("Atanan Kişi: ");
+                            todol.AtananKisi = Console.ReadLine();
+                            Console.Write("Büyüklük (XS, S, M, L, XL): ");
+                            todol.Buyukluk = Console.ReadLine().ToUpper();
+                            ToDoList.Add(todol.Baslik);
+                            ToDoList.Add(todol.Icerik);
+                            ToDoList.Add(todol.AtananKisi);
+                            ToDoList.Add(todol.Buyukluk);
+                            Console.WriteLine("Güncelleme başarılı.");
+                        }
+                        else
+                        {
+                            count++;
+                            continue;
+                        }
+                    }
+                    if (count == ToDoList.Count)
+                    {
+                        Console.WriteLine("Kayıt bulunamadı.");
+                    }
+                    break;
+
+                case "3":
+                    Console.Write("Silmek istediğiniz başlık: ");
+                    string SilinecekBaslik = Console.ReadLine();
+                    foreach (var item in ToDoList)
+                    {
+                        if (item.Contains(SilinecekBaslik))
+                        {
+                            ToDoList.Remove(todol.Baslik);
+                            ToDoList.Remove(todol.Icerik);
+                            ToDoList.Remove(todol.AtananKisi);
+                            ToDoList.Remove(todol.Buyukluk);
+                        }
+                        
+                    }
+                    break;
+                    case "4":
+                    foreach (var item in ToDoList)
+                    {
+                        Console.WriteLine("List");
+                        Console.WriteLine(item);
+                    }
+                    break;
+
+            }
+        }
+        class TodoL
+        {
+            public string Baslik { get; set; }
+            public string Icerik { get; set; }
+            public string AtananKisi { get; set; }
+            public string Buyukluk { get; set; }
+        }
+    }
+}
